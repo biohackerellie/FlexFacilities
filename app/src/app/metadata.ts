d6942ba60e9a3f473bdf8b84732dd0641049b7d4
biohackerellie <ellie@epklabs.com>
2025-11-12 07:00:34 -0700

    chore: add metadata

--- app/src/app/metadata.ts
+++ app/src/app/metadata.ts
@@ -1,19 +1,25 @@
 import type { Metadata } from 'next';
+import type { OpenGraph } from 'next/dist/lib/metadata/types/opengraph-types';
 
+export const openGraph: OpenGraph = {
+  title: 'FlexFacilities',
+  description: 'Free and Open Source facility rental software',
+  url: 'https://github.com/biohackerellie/flexfacilities',
+  siteName: 'FlexFacilities',
+  images: [
+    {
+      url: 'https://flexroster.io/FlexOSS.jpg',
+      width: 1472,
+      height: 832,
+    },
+  ],
+};
 export const meta: Metadata = {
   title: 'FlexFacilities',
   description: 'Flexible Facility Rentals',
-  keywords: ['Facility Rentals, Open Source, Facilities'],
-  creator: 'That gay gorl',
-  publisher: 'Antifa Gay Trans Mafia',
-  openGraph: {
-    title: 'FlexFacilities',
-    description: 'Open Source Facility Rentals',
-    siteName: 'FlexFacilities',
-
-    locale: 'en_US',
-    type: 'website',
-  },
+  keywords: ['Facility Rentals, Open Source, Facilities, k-12, Schools'],
+  creator: 'Ellie Kerns',
+  openGraph: openGraph,
   robots: {
     index: false,
     follow: true,
